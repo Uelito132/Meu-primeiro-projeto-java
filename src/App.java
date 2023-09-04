eeff452f914749d9ad54232a818ad15f883f5619
Welington Martins <welington.msilva12@gmail.com>
2023-09-04 11:07:23 -0300

    Projeto java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Boas vindas Turma DIO");
+        System.out.println("Boas vindas turma dio");
     }
 }
